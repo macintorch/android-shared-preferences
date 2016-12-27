2e9ee8d68589e173bcae632153b5a79ae72512da
Ainor Syahrizal <macintorch@gmail.com>
2016-12-27 09:43:04 +0800

    use sharedPreferences

--- app/src/main/java/ainor/com/my/sharedpreferences/MainActivity.java
+++ app/src/main/java/ainor/com/my/sharedpreferences/MainActivity.java
@@ -1,7 +1,10 @@
 package ainor.com.my.sharedpreferences;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("ainor.com.my.sharedpreferences", Context.MODE_PRIVATE);
+        //sharedPreferences.edit().putString("username","ainor").apply();
+
+        //String username = sharedPreferences.getString("username","");
+
+        //Log.i("username", username);
     }
 }
