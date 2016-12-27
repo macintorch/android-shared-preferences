35462acdbbba150ec8b34d29e0c181a44ee9eafd
Ainor Syahrizal <macintorch@gmail.com>
2016-12-27 09:57:02 +0800

    arraylist sharedpreferences

--- app/src/main/java/ainor/com/my/sharedpreferences/MainActivity.java
+++ app/src/main/java/ainor/com/my/sharedpreferences/MainActivity.java
@@ -6,6 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,6 +17,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         SharedPreferences sharedPreferences = this.getSharedPreferences("ainor.com.my.sharedpreferences", Context.MODE_PRIVATE);
+
+        /*
+        ArrayList<String> childrens = new ArrayList<>();
+
+        childrens.add("Asad");
+        childrens.add("Affan");
+        childrens.add("Azka");
+
+        try {
+            sharedPreferences.edit().putString("childrens", ObjectSerializer.serialize(childrens)).apply();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        */
+
+        ArrayList<String> newChildrens = new ArrayList<>();
+
+        try {
+            newChildrens = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("childrens", ObjectSerializer.serialize(new ArrayList<>())));
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+Log.i("new childrens", newChildrens.toString());
+
         //sharedPreferences.edit().putString("username","ainor").apply();
 
         //String username = sharedPreferences.getString("username","");
